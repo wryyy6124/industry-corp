2d309f3153cf4b55f9363988ecde5be9c1ee4353
ktamu <62170131+wryyy6124@users.noreply.github.com>
2025-04-12 15:00:51 +0900

    20250412_feature/headerCoding_01_SPレイアウト_ドロワーメニュー実装

--- js/script.js
+++ js/script.js
@@ -6,4 +6,10 @@ const $doc = document;
 // ------------------------------
 // DOM読み込み後に実行
 // ------------------------------
-$doc.addEventListener("DOMContentLoaded", () => {});
+$doc.addEventListener("DOMContentLoaded", () => {
+  const $Hamburger = $doc.querySelector("#Hamburger");
+
+  $Hamburger.addEventListener("click", () => {
+    $doc.body.classList.toggle("is_active");
+  });
+});
