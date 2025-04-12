c2c1ab191cf6d7aca294e44a476e9d626fe84e4d
ktamu <62170131+wryyy6124@users.noreply.github.com>
2025-04-12 16:03:31 +0900

    20250412_feature/headerCoding_02_SPレイアウト_ドロワーメニュー改修

--- js/script.js
+++ js/script.js
@@ -7,9 +7,38 @@ const $doc = document;
 // DOM読み込み後に実行
 // ------------------------------
 $doc.addEventListener("DOMContentLoaded", () => {
-  const $Hamburger = $doc.querySelector("#Hamburger");
+  // ------------------------------------------------------------
+  // ハンバーガーメニュー（SP・タブレット時のみ）
+  // ------------------------------------------------------------
 
-  $Hamburger.addEventListener("click", () => {
-    $doc.body.classList.toggle("is_active");
-  });
+  // 関数定義
+  const humbugerMenu = {
+    // DOM取得
+    $body: $doc.querySelector("#Hamburger"),
+
+    $drawer: {
+      bg: $doc.querySelector(".header__nav-body"),
+      menu: $doc.querySelector(".header__nav-list"),
+    },
+
+    // トグル
+    toggleMenu() {
+      $doc.body.classList.toggle("is_active");
+    },
+
+    // クローズのみ
+    closeMenu() {
+      $doc.body.classList.remove("is_active");
+    },
+
+    // 各イベントの起動
+    init() {
+      this.$body.addEventListener("click", this.toggleMenu);
+      this.$drawer.bg.addEventListener("click", this.closeMenu);
+      this.$drawer.menu.addEventListener("click", (e) => e.stopPropagation());
+    },
+  };
+
+  // スクリプト実行
+  humbugerMenu.init();
 });
